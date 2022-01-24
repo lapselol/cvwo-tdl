cf3a9ae44c7c00e4514929a40c30f03af0beab4e
lapselol <lapselol@gmail.com>
2022-01-24 22:37:22 +0800

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
 
     end
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
